a944df8b82fde7128ca542531c479d476f15ec2d
bsgao <brandongao2003@gmail.com>
2024-12-04 23:38:17 +0800

    initial commit

--- program/src/main.rs
+++ program/src/main.rs
@@ -1,30 +1,85 @@
-//! A simple program that takes a number `n` as input, and writes the `n-1`th and `n`th fibonacci
-//! number as an output.
-
-// These two lines are necessary for the program to properly compile.
-//
-// Under the hood, we wrap your main function with some extra code so that it behaves properly
-// inside the zkVM.
-#![no_main]
-sp1_zkvm::entrypoint!(main);
-
-use alloy_sol_types::SolType;
-use fibonacci_lib::{fibonacci, PublicValuesStruct};
+use guest::{read_models, PublicValuesStruct};
 
 pub fn main() {
-    // Read an input to the program.
+    let scaler_path = "./guest/src/model/scaler_params.json";
+    let linear_model_path = "./guest/src/model/linear_regression_params.json";
+    let ridge_model_path = "./guest/src/model/ridge_regression_params.json";
+    let poly_ridge_model_path = "./guest/src/model/polynomial_ridge_regression_params.json";
+
+    // Read the test dataset
+    let Ok((test_features, actual_amounts)) = read_test_dataset() else {
+        todo!()
+    };
+    // println!("test: {:?}", test_features);
+    // Read the models
+    let Ok((scaler, linear_model, ridge_model, poly_ridge_model)) = read_models(
+        scaler_path,
+        linear_model_path,
+        ridge_model_path,
+        poly_ridge_model_path,
+    ) else {
+        eprintln!(
+            "Error reading models: {:?}",
+            read_models(
+                scaler_path,
+                linear_model_path,
+                ridge_model_path,
+                poly_ridge_model_path
+            )
+        );
+        return; // Exit or take alternative action
+    };
+
+    let Ok(x) = flatten(test_features) else {
+        todo!()
+    };
+    let test = 3.32;
+
+    let model_input = ModelInput {
+        // test_features,
+        // actual_amounts,
+        test,
+        scaler,
+        ridge_model,
+        x,
+    };
+
+    // let (prove, verify) = guest::build_int_to_string();
     //
-    // Behind the scenes, this compiles down to a custom system call which handles reading inputs
-    // from the prover.
-    let n = sp1_zkvm::io::read::<u32>();
+    // let (output, proof) = prove(81);
+    // let is_valid = verify(proof);
+    //
+    // println!("int to string output: {:?}", output);
+    // println!("int to string valid: {}", is_valid);
+    //
+    // let (prove, verify) = guest::build_string_concat();
+    //
+    // let (output, proof) = prove(20);
+    // let is_valid = verify(proof);
+    //
+    // println!("string concat output: {:?}", output);
+    // println!("string concat valid: {}", is_valid);
+    let scaler_path = sp1_zkvm::io::read::<str>();
+    let linear_model_path = sp1_zkvm::io::read::<str>();
+    let ridge_model_path = sp1_zkvm::io::read::<str>();
+    let poly_ridge_model_path = sp1_zkvm::io::read::<str>();
 
-    // Compute the n'th fibonacci number using a function from the workspace lib crate.
-    let (a, b) = fibonacci(n);
+    let (scaler, linear_model, ridge_model, poly_ridge_model) = read_models(
+        scaler_path,
+        linear_model_path,
+        ridge_model_path,
+        poly_ridge_model_path,
+    );
+    let bytes = PublicValuesStruct::abi_encode(&PublicValuesStruct {
+        scaler_path,
+        linear_model_path,
+        ridge_model_path,
+        poly_ridge_model_path,
+        scaler,
+        linear_model,
+        ridge_model,
+        poly_ridge_model,
+    });
 
-    // Encode the public values of the program.
-    let bytes = PublicValuesStruct::abi_encode(&PublicValuesStruct { n, a, b });
-
-    // Commit to the public values of the program. The final proof will have a commitment to all the
-    // bytes that were committed to.
     sp1_zkvm::io::commit_slice(&bytes);
 }
